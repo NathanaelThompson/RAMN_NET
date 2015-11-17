993bd1367146a912bb6ddfabe0712dada1d95485
Nathanael Thompson <nathanael.thompson90@gmail.com>
2015-11-17 14:22:18 -0500

    NB Trash

--- ClientNode/src/CNUI/clientnode/ClientNodeUI.java
+++ ClientNode/src/CNUI/clientnode/ClientNodeUI.java
@@ -379,6 +379,7 @@ public class ClientNodeUI extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String args[])throws IOException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
